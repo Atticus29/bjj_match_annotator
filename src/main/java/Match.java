5fd3ec2e35c7affbab6cfd446f751a513a93586c
Mark Fisher <mark.aaron.fisher@gmail.com>
2017-03-29 13:58:55 -0700

    add getters and test thereof

--- src/main/java/Match.java
+++ src/main/java/Match.java
@@ -24,4 +24,59 @@ public class Match {
     this.athlete_2_belt = a2Belt;
   }
 
+  @Override
+  public boolean equals(Object match2){
+    if(! match2 instanceof Match){
+      return false;
+    } else{
+      Match newMatch = (Match) match2;
+      return this.getName().equals(newMatch.getName()) &&
+      this.getHost_org().equals(newMatch.getHost_org()) &&
+      this.getLocation().equals(newMatch.getLocation()) &&
+      this.getDate().equals(newMatch.getDate()) &&
+      this.getAthlete_1_name().equals(newMatch.getAthlete_1_name()) &&
+      this.getAthlete_2_name().equals(newMatch.getAthlete_2_name()) &&
+      this.getWeight_class().equals(newMatch.ggetWeight_class()) &&
+      this.getAthlete_1_belt().equals(newMatch.getAthlete_1_belt()) &&
+      this
+    }
+  }
+
+  public void setId(int id){
+    this.id = id;
+  }
+
+  public int getId(){
+    return this.id;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public String getHost_org(){
+    return this.host_org;
+  }
+  public String getLocation(){
+    return this.location;
+  }
+  public String getDate(){
+    return this.date;
+  }
+  public String getAthlete_1_name(){
+    return this.athlete_1_name;
+  }
+  public String getAthlete_2_name(){
+    return this.athlete_2_name;
+  }
+  public String getWeight_class(){
+    return this.weight_class;
+  }
+  public String getAthlete_1_belt(){
+    return this.athlete_1_belt;
+  }
+  public String getAthlete_2_belt(){
+    return this.athlete_2_belt;
+  }
+
 }
--- src/test/java/MatchTest.java
+++ src/test/java/MatchTest.java
@@ -9,6 +9,7 @@ public class MatchTest {
   public void setUp() {
     testMatch = new Match("Finney_vs_Fisher_Bantam_Worlds_Gi_Final", "IBJJF Gi Worlds","Long Beach, CA" ,"05-01-2017", "Mark Fisher", "Chris Finney", "Bantam", "Brown",
     "Brown");
+    testMatch.save();
   }
 
   @Rule
@@ -18,5 +19,30 @@ public class MatchTest {
   public void match_instantiatesCorrectly(){
     assertTrue(testMatch instanceof Match);
   }
+
+  @Test
+  public void getters_returnExpectedValues_true(){
+    assertEquals("Finney_vs_Fisher_Bantam_Worlds_Gi_Final", testMatch.getName());
+    assertEquals("IBJJF Gi Worlds", testMatch.getHost_org());
+    assertEquals("Long Beach, CA", testMatch.getLocation());
+    assertEquals("05-01-2017", testMatch.getDate());
+    assertEquals("Mark Fisher", testMatch.getAthlete_1_name());
+    assertEquals("Chris Finney", testMatch.getAthlete_2_name());
+    assertEquals("Bantam", testMatch.getWeight_class());
+    assertEquals("Brown", testMatch.getAthlete_1_belt());
+    assertEquals("Brown", testMatch.getAthlete_2_belt());
+    testMatch.setId(1);
+    assertEquals(4, testMatch.getId());
+  }
+
+  @Test
+  public void equals_returnsWhetherTwoMatchesAreIdentical_true(){
+
+  }
+
+  // @Test
+  // public void all_returnsListOfMatches_List(){
+  //
+  // }
 
 }
