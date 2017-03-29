97cc41e7f096e0067646fb01a75b16111737eb3c
Chris Finney and Mark Fisher <chris@>
2017-03-29 15:29:40 -0700

    create Move class and constructor variables, getter and additional methods to successfully pass instantiation tests and equals test

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -1,9 +1,64 @@
+
 import org.sql2o.*;
 
 public class Move {
+  private int id;
+  private int matchId;
+  private String name;
+  private String description;
+  private String difficulty;
 
-  public Move() {
 
+  public Move(int matchId, String name, String description, String difficulty) {
+    this.matchId = matchId;
+    this.name = name;
+    this.description = description;
+    this.difficulty = difficulty;
   }
 
+  public int getMatchId() {
+    return matchId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getDifficulty() {
+    return difficulty;
+  }
+
+  public static List<Move> all() {
+    String sql = "SELECT * FROM moves;";
+    try(Connection con = DB.sql2o.open())
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO moves (matchId, name, description, difficulty) VALUES (:matchId, :name, :description, :difficulty)";
+        con.createQuery(sql)
+        .addParameter("matchId", this.matchId)
+        .addParameter("name", this.name)
+        .addParameter("description", this.description)
+        .addParameter("difficulty", this.difficulty)
+        .executeUpdate();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherMove) {
+    if (!(otherMove instanceof Move)) {
+      return false;
+    } else {
+      Move newMove = (Move) otherMove;
+      return this.getName().equals(newMove.getName()) &&
+             this.getDescription().equals(newMove.getDescription()) &&
+             this.getDifficulty().equals(newMove.getDifficulty()) &&
+             this.getMatchId() == newMove.getMatchId();
+    }
+  }
 }
--- src/test/java/MoveTest.java
+++ src/test/java/MoveTest.java
@@ -4,17 +4,68 @@ import static org.junit.Assert.*;
 
 public class MoveTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/java_jitsu_test", null, null);
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Move_instantiatesCorrectly_true() {
+    Move myMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    assertEquals(true, myMove instanceof Move);
   }
 
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
+  @Test
+  public void Move_instantiatesWithName_String() {
+    Move myMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    assertEquals("guillotine choke", myMove.getName());
   }
 
+  @Test
+  public void Move_instantiatesWithDescription_String() {
+    Move myMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    assertEquals("forward-facing choke hold", myMove.getDescription());
+  }
+
+  @Test
+  public void Move_instantatiesWithDifficulty_String() {
+    Move myMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    assertEquals("beginner", myMove.getDifficulty());
+  }
+
+  @Test
+  public void Move_instantiatesWithMatchId() {
+    Move myMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    assertEquals(1, myMove.getMatchId());
+  }
+
+  @Test
+  public void equals_returnTrueIfPropertiesAreTheSame() {
+    Move firstMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    Move secondMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    assertTrue(firstMove.equals(secondMove));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfMove_true() {
+    Move firstMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+    firstMove.save();
+    Move secondMove = new Move(1, "armbar", "forced hyperextension of the shoulder and elbow joints", "intermediate");
+    secondMove.save();
+    assertEquals(true, Move.all().get(0).equals(firstMove));
+    assertEquals(true, Move.all().get(1).equals(secondMove));
+  }
+
+  // @Test
+  // public void save_() {
+  //   Move myMove = new Move(1, "guillotine choke", "forward-facing choke hold", "beginner");
+  //   myMove.save();
+  //
+  // }
 }
+
+// methods to create:
+//
+// static all()
+// static find()
+// save()
+// update()
+// delete()
