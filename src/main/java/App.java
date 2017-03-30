6d6c6dc545ceffd18d7366e7b96b662a99c0ac2f
Chris Finney and Mark Fisher <chris@>
2017-03-30 16:52:18 -0700

    add move edit, delete, and back buttons with correct routing and behavior. Will need to fix move.update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,20 +25,24 @@ public class App {
 
     post("/matches/new/moves/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      String name = request.queryParams("match-name");
-      String host = request.queryParams("host-org");
-      String location = request.queryParams("location");
-      String date = request.queryParams("date");
-      String athleteOneName = request.queryParams("athlete-1-name");
-      String athleteTwoName = request.queryParams("athlete-2-name");
-      String weightClass = request.queryParams("weight-class");
-      String aOneBelt = request.queryParams("athlete-1-belt");
-      String aTwoBelt = request.queryParams("athlete-2-belt");
-      String matchUrl = request.queryParams("match-url");
-      Match newMatch = new Match(name, host, location, date, athleteOneName, athleteTwoName, weightClass, aOneBelt, aTwoBelt, matchUrl);
-      newMatch.save();
       String origin = request.queryParams("origin");
-      model.put("match", newMatch);
+      if (!origin.equals("add-another")) {
+        String name = request.queryParams("match-name");
+        String host = request.queryParams("host-org");
+        String location = request.queryParams("location");
+        String date = request.queryParams("date");
+        String athleteOneName = request.queryParams("athlete-1-name");
+        String athleteTwoName = request.queryParams("athlete-2-name");
+        String weightClass = request.queryParams("weight-class");
+        String aOneBelt = request.queryParams("athlete-1-belt");
+        String aTwoBelt = request.queryParams("athlete-2-belt");
+        String matchUrl = request.queryParams("match-url");
+        Match newMatch = new Match(name, host, location, date, athleteOneName, athleteTwoName, weightClass, aOneBelt, aTwoBelt, matchUrl);
+        newMatch.save();
+        model.put("match", newMatch);
+      // } else {
+      //   model.put("match", request.queryParams("match"));
+      }
       model.put("origin", origin);
       model.put("template", "templates/move-form.vtl");
       return new ModelAndView(model, layout);
@@ -51,8 +55,7 @@ public class App {
       int matchid = Integer.parseInt(request.params(":matchid"));
       if(!origin.equals("update")){
         String name = request.queryParams("move-name");
-        String description = request.queryParams("description"
-        );
+        String description = request.queryParams("description");
         String difficulty = request.queryParams("difficulty");
         Move newMove = new Move(matchid, name, description, difficulty);
         newMove.save();
@@ -92,7 +95,19 @@ public class App {
       int matchidInt = Integer.parseInt(request.params(":matchid"));
       Match match = Match.find(matchidInt);
       model.put("match", match);
+      model.put("moves", match.getMoves(matchidInt));
       model.put("template", "templates/match.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/matches/:matchid/moves/:moveid/delete", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchidInt);
+      int moveID = Integer.parseInt(request.queryParams("moveid"));
+      Move move = Move.find(moveID);
+      move.delete();
+      response.redirect("/matches/" + match.getId());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -106,6 +121,19 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/matches/:matchid/moves/:moveid/edit", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchidInt);
+      model.put("match", match);
+      int moveID = Integer.parseInt(request.queryParams("moveid"));
+      Move move = Move.find(moveID);
+      model.put("origin", "edit");
+      model.put("move", move);
+      model.put("template", "templates/move-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/matches/:matchid/edit", (request, response) ->{
       Map<String, Object> model = new HashMap<String, Object>();
       int matchidInt = Integer.parseInt(request.params(":matchid"));
@@ -116,6 +144,27 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/matches/:matchid/moves/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchidInt);
+      model.put("origin", "new-move");
+      // model.put("origin", request.queryParams("origin"));
+      model.put("match", match);
+      model.put("template", "templates/move-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("matches/:matchid/moves/:moveid", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      int moveidInt = Integer.parseInt(request.params(":moveid"));
+      Move move = Move.find(moveidInt);
+      model.put("match", Match.find(matchidInt));
+      model.put("move", move);
+      model.put("template", "templates/move.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
 }
