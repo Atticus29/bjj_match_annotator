0d4b0c8d8f16f6915415c87830dc9b37a28365f8
Chris Finney and Mark Fisher <mark@>
2017-03-30 15:13:28 -0700

    add and fix routes for editing matches and deleting matches

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,7 @@ public class App {
 
     get("/matches/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("origin", "new");
       model.put("template", "templates/match-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -46,19 +47,75 @@ public class App {
     post("/matches/:matchid", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       System.out.println("all query params are " + request.queryParams());
+      String origin = request.queryParams("origin");
       int matchid = Integer.parseInt(request.params(":matchid"));
-      String name = request.queryParams("move-name");
-      String description = request.queryParams("description"
-      );
-      String difficulty = request.queryParams("difficulty");
-      Move newMove = new Move(matchid, name, description, difficulty);
-      newMove.save();
-      // model.put("matches", Match.all());
+      if(!origin.equals("update")){
+        String name = request.queryParams("move-name");
+        String description = request.queryParams("description"
+        );
+        String difficulty = request.queryParams("difficulty");
+        Move newMove = new Move(matchid, name, description, difficulty);
+        newMove.save();
+        model.put("moves", Match.find(matchid).getMoves(matchid));
+      } else{
+        Match match = Match.find(matchid);
+        String name = request.queryParams("match-name");
+        String hostOrg = request.queryParams("host-org");
+        String location = request.queryParams("location");
+        String date = request.queryParams("date");
+        String a1Name = request.queryParams("athlete-1-name");
+        String a2Name = request.queryParams("athlete-2-name");
+        String weightClass = request.queryParams("weight-class");
+        String a1Belt = request.queryParams("athlete-1-belt");
+        String a2Belt = request.queryParams("athlete-2-belt");
+        String matchURL = request.queryParams("match-url");
+        match.update(name, hostOrg, location, date, a1Name, a2Name, weightClass, a1Belt, a2Belt, matchURL);
+      }
       model.put("match", Match.find(matchid));
-
-      // model.put("matchid", matchid);
       model.put("template", "templates/match.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/matches/:matchid/edit", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("origin", "update");
+      int matchid = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchid);
+      model.put("match", match);
+      model.put("template", "templates/match-form.vtl");
+      // response.redirect("/matches/new/moves/new");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/matches/:matchid", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchidInt);
+      model.put("match", match);
+      model.put("template", "templates/match.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/matches/:matchid/delete", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchidInt);
+      match.delete();
+      response.redirect("/");
+      // model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/matches/:matchid/edit", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      int matchidInt = Integer.parseInt(request.params(":matchid"));
+      Match match = Match.find(matchidInt);
+      model.put("origin", "edit");
+      model.put("match", match);
+      model.put("template", "templates/match-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
+
 }
